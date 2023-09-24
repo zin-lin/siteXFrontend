a2f449985381d6a707f72af3a09da6637d559f6a
zin-htun <zinlinhtun34@gmail.com>
2023-09-24 10:37:56 +0100

    Necessary Libs added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+
 
 function App() {
+
+  const [sta, setSta] = useState("")
+  fetch('freeman')
+      .then(res => res.text()).then(data => setSta(data)).catch(err => err)
+
   return (
     <div className="App">
       <header className="App-header">
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -15,7 +23,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {sta}
         </a>
       </header>
     </div>
